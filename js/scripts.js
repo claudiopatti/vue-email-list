5bbf3674b7c4f678c0ab0684d2a855eec55ef836
claudiopatti <claudiopatti1997@gmail.com>
2024-09-02 17:33:24 +0200

    aggiunte email tramite vue e inportazione di axios

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,27 @@ const { createApp } = Vue
     createApp({
         data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            emails: [
+                
+            ],
+            email : '',
         }
+    },
+    created () {
+        console.log(this.emails)
+            
+        for (let i = 0; i < 10; i++) {
+            axios 
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+                    this.email = resp.data.response;
+                    this.emails.push(this.email);
+                    this.email = '';
+                    console.log(resp.data.response);
+                })
+        }
+            console.log(this.emails)
         }
     }).mount('#app')
+
